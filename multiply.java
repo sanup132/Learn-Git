1e4e44756cae9e38d1e44c7d71a263c868f9d51e
Sanup kumar <sanup.cse@gmail.com>
2023-05-12 09:58:30 +0530

    text uploaded

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 8;
+
+    int c = a * b;
+    System.out.println(c);
+  }
+}
---
+++ sum.java
@@ -0,0 +1,9 @@
+class sum {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 9;
+    System.out.println(a + b);
+    System.out.println(a + b);
+
+  }
+}
